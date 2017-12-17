33fa16b8b91dd9590d6794da53206059b0a0da5d
Clancy-W <34616988+Clancy-W@users.noreply.github.com>
2017-12-17 19:34:29 +1100

    Added Comments

--- 2017-01/JavaScript.js
+++ 2017-01/JavaScript.js
@@ -1,20 +1,22 @@
 function part1(inp) {
 	var pos = 0;
 	var o = 0;
-	for (var i == 0; i < inp.length; i ++) {
+	for (var i == 0; i < inp.length; i ++) { //Loop through input
 		pos += 1;
-		if (pos >= inp.length) {pos -= inp.length;}
-		if(input.charAt(i) == input.charAt(pos)) {o += parseInt(input.charAt(i));}}
-	return o;
-
+		if (pos >= inp.length) {pos -= inp.length;} //Make circular
+		if(input.charAt(i) == input.charAt(pos)) {o += parseInt(input.charAt(i));}} //Add number if equal
+	return o; //Send back answer to part 1
+}
+	
 function part2(inp) {
 	var pos = 0;
 	var o = 0;
-	for (var i == 0; i < inp.length; i ++) {
+	for (var i == 0; i < inp.length; i ++) { //Loop through input
 		pos = i + inp.length / 2;
-		if (pos >= inp.length) {pos -= inp.length;}
-		if(input.charAt(i) == input.charAt(pos)) {o += parseInt(input.charAt(i));}}
-	return o;
+		if (pos >= inp.length) {pos -= inp.length;} //Make circular
+		if(input.charAt(i) == input.charAt(pos)) {o += parseInt(input.charAt(i));}} //Add number if equal
+	return o; //Send back answer to part 2
+}
 
-console.log("Part 1: " + part1(1111))
+console.log("Part 1: " + part1(1111));
 console.log("Part 2: " + part2(1212))
