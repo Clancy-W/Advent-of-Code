4032c9cc62b3c2f4beee0e86a56a2f19a6da1651
Clancy-W <34616988+Clancy-W@users.noreply.github.com>
2017-12-17 17:52:32 +1100

    Made more readable

--- 2017-17.py
+++ 2017-17.py
@@ -1,24 +1,29 @@
-def part1(l):
+def part1(inp):
+    #Initialization
     pos = 0
     arr = [0]
-    for i in range(1, 2018):
-        pos = (pos + l) % len(arr)
+    
+    for i in range(1, 2018): #Loops through to 2017
+        pos = (pos + inp) % len(arr)
         arr = arr[:pos+1] + [i] + arr[pos+1:]
-        pos = pos + 1
+        pos += 1
 
-    return (arr[arr.index(2017) + 1])
+    return (arr[arr.index(2017) + 1]) #Returns answer to part 1
 
-def part2(l):
+def part2(inp):
+    #Initialization
     pos = 0
     arr = [0]
-    for i in range(1, 50000000):
-        pos = (pos + l) % len(arr)
-        if pos == 0:
+    
+    
+    for i in range(1, 50000000): #Loops through to 5 billion
+        pos = (pos + inp) % len(arr)
+        if pos == 0: #Only add i if position at zero
             arr = [0] + [i] + arr[1:]
         else:
             arr += [0]
-        pos = pos + 1
-    return (arr[arr.index(0) + 1])
+        pos += 1
+    return (arr[1]) #Returns answer to part 2
 
-print(part1(324))
-print(part2(324))
+print("Part 1:", part1(324))
+print("Part 2:", part2(324))
